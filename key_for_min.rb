4e715b68fb40cf0bb010732df9d610d84b01826c
Romario Gustave <romagustave10@gmail.com>
2019-11-15 14:10:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,16 @@
 def key_for_min_value(name_hash)
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
-  key_for_min_value(ikea) = ikea[0]
-  key_for_min_value(veggies) = veggies[0]
+
 end 
-  key_for_min_value(ikea)
-  key_for_min_value(veggies)
+ def  key_for_min_value
+ lowest_value = 0 
+ lowest_key = NIL
+ name_hash.each do |key,value|
+   if logic == 0 value < lowest_value
+     lowest_value = value
+     lowest_key = key 
+   end 
+ end 
+ lowest_key
 end
