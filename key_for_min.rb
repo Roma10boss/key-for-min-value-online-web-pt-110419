393370a166294091506015e66930e6be12906c82
Romario Gustave <romagustave10@gmail.com>
2019-11-15 14:04:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,10 @@
 
 def key_for_min_value(name_hash)
 ikea = {:chair => 25, :table => 85, :mattress => 450}
+veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+  key_for_min_value(ikea) = ikea[0]
+  key_for_min_value(veggies) = veggies[0]
+end 
+  key_for_min_value(ikea)
+  key_for_min_value(veggies)
 end
